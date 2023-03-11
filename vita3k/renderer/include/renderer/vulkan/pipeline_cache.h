5f83447d120901ac30666dd217d9b67c348ada10
bookmist <roshst@yandex.ru>
2023-03-11 19:53:02 +0300

    rendered/vulkan/pipeline_cache: multithreaded precompilation of shaders

--- vita3k/renderer/include/renderer/vulkan/pipeline_cache.h
+++ vita3k/renderer/include/renderer/vulkan/pipeline_cache.h
@@ -50,13 +50,13 @@ private:
     static constexpr int pipeline_cache_save_delay = 30;
 
     vk::PipelineCache pipeline_cache;
-
     // first index: 1 if depth-stencil is force loaded, 0 otherwise
     // second index: 1 if depth-stencil is force stored, 0 otherwise
     std::map<vk::Format, vk::RenderPass> render_passes[2][2];
     // render passes used along shader interlock
     std::map<vk::Format, vk::RenderPass> shader_interlock_pass;
     std::map<Sha256Hash, vk::ShaderModule> shaders;
+    std::mutex shaders_mutex = {};
     std::unordered_map<uint64_t, vk::Pipeline> pipelines;
 
     // temp vars used to store the result computed by auxialiary functions before createPipeline is called
