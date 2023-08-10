eb77f2a07af2ade51d5b79d2c048b033c3349e9a
bookmist <roshst@yandex.ru>
2023-08-10 13:35:42 +0300

    vita3k: add non-standart lle modules to lle modules list. allow to lle user-level kernel

--- vita3k/interface.cpp
+++ vita3k/interface.cpp
@@ -495,6 +495,18 @@ static ExitCode load_app_impl(SceUID &main_module_id, EmuEnvState &emuenv) {
                 emuenv.kernel.loaded_sysmodules[module_id] = {};
         }
     };
+    if (is_lle_module("libkernel", emuenv))
+        lib_load_list.emplace_back("os0:us/libkernel.suprx");
+    if (is_lle_module("driver_us", emuenv))
+        lib_load_list.emplace_back("os0:us/driver_us.suprx");
+    if (is_lle_module("avcodec_us", emuenv))
+        lib_load_list.emplace_back("os0:us/avcodec_us.suprx");
+    if (is_lle_module("libgpu_es4", emuenv))
+        lib_load_list.emplace_back("os0:us/libgpu_es4.suprx");
+    if (is_lle_module("libgxm_es4", emuenv))
+        lib_load_list.emplace_back("os0:us/libgxm_es4.suprx");
+    if (is_lle_module("libgxm_dbg_es4", emuenv))
+        lib_load_list.emplace_back("os0:us/libgxm_dbg_es4.suprx"); // if DEVELOPMENT_MODE dipsw is set
     add_preload_module(0x00010000, SCE_SYSMODULE_INVALID, "libc", true);
     add_preload_module(0x00020000, SCE_SYSMODULE_DBG, "libdbg", false);
     add_preload_module(0x00080000, SCE_SYSMODULE_INVALID, "libshellsvc", false);
@@ -898,7 +910,6 @@ ExitCode load_app(int32_t &main_module_id, EmuEnvState &emuenv) {
         emuenv.kernel.debugger.wait_for_debugger = true;
         server_open(emuenv);
     }
-
 #if USE_DISCORD
     if (emuenv.cfg.discord_rich_presence)
         discordrpc::update_presence(emuenv.io.title_id, emuenv.current_app_title);
