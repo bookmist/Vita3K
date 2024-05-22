bf52ffc14ea09b4a9026c7ac315453f91dc90b0e
bookmist <roshst@yandex.ru>
2024-05-22 12:37:21 +0300

    taihen: intermediate commit

--- vita3k/modules/taihen/taihen.cpp
+++ vita3k/modules/taihen/taihen.cpp
@@ -46,8 +46,8 @@ TRACY_MODULE_NAME(taihen);
 
 #include <stdint.h>
 
-struct SCE_TYPE(sce_module_exports);
-struct SCE_TYPE(sce_module_imports);
+// struct SCE_TYPE(sce_module_exports);
+// struct SCE_TYPE(sce_module_imports);
 
 typedef struct SCE_TYPE(sce_module_exports) {
     uint16_t size; /* Size of this struct, set to 0x20 */
@@ -57,15 +57,15 @@ typedef struct SCE_TYPE(sce_module_exports) {
     uint32_t num_syms_vars; /* Number of variable exports */
     uint32_t num_syms_tls_vars; /* Number of TLS variable exports */
     uint32_t library_nid; /* NID of this library */
-    SCE_PTR(const char *)
+    SCE_PTR(const char)
     library_name; /* Pointer to name of this library */
-    SCE_PTR(uint32_t *)
+    SCE_PTR(uint32_t)
     nid_table; /* Pointer to array of 32-bit NIDs to export */
-    SCE_PTR(const void **)
+    SCE_PTR(const Ptr<void>)
     entry_table; /* Pointer to array of data pointers for each NID */
 } SCE_TYPE(sce_module_exports);
 
-typedef struct SCE_TYPE(sce_module_imports) {
+typedef struct SCE_TYPE(sce_module_imports_long) {
     uint16_t size; /* Size of this struct, set to 0x34 */
     uint16_t version; /* Set to 0x1 */
     uint16_t flags; /* Set to 0x0 */
@@ -75,22 +75,22 @@ typedef struct SCE_TYPE(sce_module_imports) {
 
     uint32_t reserved1;
     uint32_t library_nid; /* NID of library to import */
-    SCE_PTR(const char *)
+    SCE_PTR(const char)
     library_name; /* Pointer to name of imported library, for debugging */
     uint32_t reserved2;
-    SCE_PTR(uint32_t *)
+    SCE_PTR(uint32_t)
     func_nid_table; /* Pointer to array of function NIDs to import */
-    SCE_PTR(const void **)
+    SCE_PTR(const Ptr<void>)
     func_entry_table; /* Pointer to array of stub functions to fill */
-    SCE_PTR(uint32_t *)
+    SCE_PTR(uint32_t)
     var_nid_table; /* Pointer to array of variable NIDs to import */
-    SCE_PTR(const void **)
+    SCE_PTR(const Ptr<void>)
     var_entry_table; /* Pointer to array of data pointers to write to */
-    SCE_PTR(uint32_t *)
+    SCE_PTR(uint32_t)
     tls_var_nid_table; /* Pointer to array of TLS variable NIDs to import */
-    SCE_PTR(const void **)
+    SCE_PTR(const Ptr<void>)
     tls_var_entry_table; /* Pointer to array of data pointers to write to */
-} SCE_TYPE(sce_module_imports);
+} SCE_TYPE(sce_module_imports_long);
 
 /* alternative module imports struct with a size of 0x24 */
 typedef struct SCE_TYPE(sce_module_imports_short) {
@@ -102,17 +102,23 @@ typedef struct SCE_TYPE(sce_module_imports_short) {
     uint16_t num_syms_tls_vars; /* Number of TLS variable imports */
 
     uint32_t library_nid; /* NID of library to import */
-    SCE_PTR(const char *)
+    SCE_PTR(const char)
     library_name; /* Pointer to name of imported library, for debugging */
-    SCE_PTR(uint32_t *)
+    SCE_PTR(uint32_t)
     func_nid_table; /* Pointer to array of function NIDs to import */
-    SCE_PTR(const void **)
+    SCE_PTR(const Ptr<void>)
     func_entry_table; /* Pointer to array of stub functions to fill */
-    SCE_PTR(uint32_t *)
+    SCE_PTR(uint32_t)
     var_nid_table; /* Pointer to array of variable NIDs to import */
-    SCE_PTR(const void **)
+    SCE_PTR(const Ptr<void>)
     var_entry_table; /* Pointer to array of data pointers to write to */
 } SCE_TYPE(sce_module_imports_short);
+
+typedef union sce_module_imports_raw {
+    uint16_t size;
+    sce_module_imports_long_raw type1;
+    sce_module_imports_short_raw type2;
+} sce_module_imports_raw_t;
 
 struct sce_module_info_raw { // size is 0x5C-bytes
     uint16_t attributes;
@@ -369,21 +375,282 @@ enum TaiErrorCode : uint32_t {
     TAI_ERROR_BLOCKING = 0x9001000D
 };
 
+/**
+ * @brief      Gets a loaded module by name or NID or both
+ *
+ *             If `name` is NULL, then only the NID is used to locate the loaded
+ *             module. If `name` is not NULL then it will be used to lookup the
+ *             loaded module. If NID is not `TAI_IGNORE_MODULE_NID`, then it
+ *             will be used in the lookup too. If `name` is NULL and NID is
+ *             `TAI_IGNORE_MODULE_NID` then the first module loaded for the
+ *             process will be returned.
+ *
+ * @param[in]  pid   The pid
+ * @param[in]  name  The name to lookup. Can be NULL.
+ * @param[in]  nid   The nid to lookup. Can be `TAI_IGNORE_MODULE_NID`.
+ * @param[out] info  The information
+ *
+ * @return     Zero on success, < 0 on error
+ *             - TAI_ERROR_INVALID_MODULE if both `name` and NID are undefined
+ *               and `pid` is KERNEL_PID.
+ *             - TAI_ERROR_MODULE_OVERFLOW if there are more than
+ *               `MOD_LIST_SIZE` modules loaded for the process. This is a
+ *               system error and should be reported.
+ */
+int module_get_by_name_nid(EmuEnvState &emuenv, SceUID pid, const char *module_name, uint32_t module_nid, tai_module_info_t *info) {
+    if (info->size != sizeof(tai_module_info_t)) {
+        LOG_ERROR("Structure size too small: %d", info->size);
+        return TAI_ERROR_SYSTEM;
+    }
+
+    int module_id = 0;
+    if (module_name == nullptr && module_nid == TAI_IGNORE_MODULE_NID) {
+        for (auto &[module_id_, module] : emuenv.kernel.loaded_modules) {
+            if (module->info.path == "app0:" + emuenv.self_path) {
+                module_id = module_id_;
+            }
+        }
+    } else if (module_nid != TAI_IGNORE_MODULE_NID) {
+        for (auto &[module_id_, module_info] : emuenv.kernel.loaded_modules) {
+            const sce_module_info_raw *int_mod_info = reinterpret_cast<const sce_module_info_raw *>(module_info->info_segment_address.get(emuenv.mem) + module_info->info_offset);
+            if (int_mod_info->module_nid == module_nid) {
+                module_id = module_id_;
+            }
+        }
+
+    } else {
+        for (auto &[module_id_, module] : emuenv.kernel.loaded_modules) {
+            if (strncmp(module->info.module_name, module_name, sizeof(module->info.module_name)) == 0) {
+                module_id = module_id_;
+            }
+        }
+    }
+    if (module_id == 0) {
+        return TAI_ERROR_NOT_FOUND;
+    }
+    auto &module_info = emuenv.kernel.loaded_modules[module_id];
+    const sce_module_info_raw *int_mod_info = reinterpret_cast<const sce_module_info_raw *>(module_info->info_segment_address.get(emuenv.mem) + module_info->info_offset);
+    info->modid = module_id;
+    info->module_nid = int_mod_info->module_nid;
+    info->exports_start = int_mod_info->export_top;
+    info->exports_end = int_mod_info->export_end;
+    info->imports_start = int_mod_info->import_top;
+    info->imports_end = int_mod_info->import_end;
+    strncpy(info->name, int_mod_info->name, 27);
+    info->name[26] = '\0';
+    return TAI_SUCCESS;
+}
+
+/**
+ * @brief      Gets an imported function stub address
+ *
+ * @param[in]  pid            The pid
+ * @param[in]  modname        The name of the module importing the function
+ * @param[in]  target_libnid  The target's library NID. Can be `TAI_ANY_LIBRARY`
+ * @param[in]  funcnid        The target's function NID
+ * @param[out] stub           Output address to stub calling the imported
+ *                            function
+ *
+ * @return     Zero on success, < 0 on error
+ */
+int module_get_import_func(EmuEnvState &emuenv, SceUID thread_id, SceUID pid, const char *modname, uint32_t target_libnid, uint32_t funcnid, Ptr<void> *stub) {
+    tai_module_info_t info;
+    int i;
+
+    LOG("Getting import for pid:%x, modname:%s, target_libnid:%x, funcnid:%x", pid, modname, target_libnid, funcnid);
+    info.size = sizeof(info);
+    if (module_get_by_name_nid(emuenv, pid, modname, TAI_IGNORE_MODULE_NID, &info) < 0) {
+        LOG("Failed to find module: {}", modname);
+        return TAI_ERROR_NOT_FOUND;
+    }
+
+    auto info_segment_address = emuenv.kernel.loaded_modules[info.modid]->info_segment_address.address();
+    LOG_TRACE("imports_start: {:#X}; imports_end: {:#X}", info.imports_start.address(), info.imports_end.address());
+    for (Address cur = info.imports_start.address() + info_segment_address; cur < info.imports_end.address() + info_segment_address;) {
+        Ptr<sce_module_imports_raw> import_ptr(cur);
+        if (!import_ptr.valid(emuenv.mem)) {
+            LOG_ERROR("Invalid import pointer: {:#X}", cur);
+            return TAI_ERROR_SYSTEM;
+        }
+        auto import = import_ptr.get(emuenv.mem);
+        // LOG("import size is 0x%04X", import->size);
+        if (import->size == sizeof(sce_module_imports_long_raw)) {
+            if (target_libnid == TAI_ANY_LIBRARY || import->type1.library_nid == target_libnid) {
+                for (i = 0; i < import->type1.num_syms_funcs; i++) {
+                    if (import->type1.func_nid_table.get(emuenv.mem)[i] == funcnid) {
+                        *stub = import->type1.func_entry_table.get(emuenv.mem)[i];
+                        LOG("found function stub address: {}", *stub);
+                        return TAI_SUCCESS;
+                    }
+                }
+            }
+        } else if (import->size == sizeof(struct sce_module_imports_short_raw)) {
+            if (target_libnid == TAI_ANY_LIBRARY || import->type2.library_nid == target_libnid) {
+                for (i = 0; i < import->type2.num_syms_funcs; i++) {
+                    if (import->type2.func_nid_table.get(emuenv.mem)[i] == funcnid) {
+                        *stub = import->type2.func_entry_table.get(emuenv.mem)[i];
+                        LOG("found function stub address: 0x%08X", *stub);
+                        return TAI_SUCCESS;
+                    }
+                }
+            }
+        } else {
+            LOG_ERROR("Invalid import size: {}", import->size);
+        }
+        cur += import->size;
+    }
+
+    return TAI_ERROR_NOT_FOUND;
+}
+
+/**
+ * @brief      Inserts a hook given an absolute address and PID of the function
+ *
+ * @param[out] p_hook     Outputs a reference object if successful
+ * @param[in]  pid        PID of the address space to hook
+ * @param      dest_func  The destination function
+ * @param[in]  hook_func  The hook function
+ *
+ * @return     UID for the hook on success, < 0 on error
+ */
+SceUID tai_hook_func_abs(EmuEnvState &emuenv, tai_hook_ref_t *p_hook, SceUID pid, Ptr<void> dest_func, Ptr<const void> hook_func) {
+    LOG_CONSOLE(taiHookFunctionImportForKernel, p_hook, pid, dest_func, hook_func);
+    auto export_name = __FUNCTION__;
+    return UNIMPLEMENTED();
+    /*
+  SceCreateUidObjOpt opt;
+  tai_patch_t *patch, *tmp;
+  tai_hook_t *hook;
+  int ret;
+  struct slab_chain *slab;
+  uintptr_t exe_addr;
+
+  LOG("Hooking %p to %p for pid %x", hook_func, dest_func, pid);
+  if (hook_func >= MEM_SHARED_START) {
+    if (pid == KERNEL_PID) {
+      return TAI_ERROR_INVALID_KERNEL_ADDR; // invalid hook address
+    } else {
+      return TAI_ERROR_NOT_IMPLEMENTED; // TODO: add support for this
+    }
+  }
+
+  hook = NULL;
+  if (pid == KERNEL_PID) {
+    ret = ksceKernelCreateUidObj(&g_taihen_class, "tai_patch_hook", NULL, (SceObjectBase **)&patch);
+  } else {
+    memset(&opt, 0, sizeof(opt));
+    opt.flags = 8;
+    opt.pid = pid;
+    ret = ksceKernelCreateUidObj(&g_taihen_class, "tai_patch_hook_user", &opt, (SceObjectBase **)&patch);
+  }
+  LOG("ksceKernelCreateUidObj(tai_patch_hook): 0x%08X, %p", ret, patch);
+  if (ret < 0) {
+    return ret;
+  }
+
+  ksceKernelLockMutex(g_hooks_lock, 1, NULL);
+  patch->type = HOOKS;
+  patch->uid = ret;
+  patch->pid = pid;
+  patch->addr = FUNC_TO_UINTPTR_T(dest_func);
+  patch->size = FUNC_SAVE_SIZE;
+  patch->next = NULL;
+  patch->data.hooks.func = dest_func;
+  patch->data.hooks.saved = NULL;
+  patch->data.hooks.head = NULL;
+  if (proc_map_try_insert(g_map, patch, &tmp) < 1) {
+    ret = ksceKernelDeleteUid(patch->uid);
+    LOG("ksceKernelDeleteUid(old): 0x%08X", ret);
+    if (tmp == NULL || tmp->type != HOOKS) {
+      // error
+      LOG("this hook overlaps an existing hook");
+      ret = TAI_ERROR_PATCH_EXISTS;
+      goto err;
+    } else {
+      // we have an existing patch
+      LOG("found existing patch %p, discarding %p", tmp, patch);
+      patch = tmp;
+    }
+  }
+
+  hook = slab_alloc(patch->slab, &exe_addr);
+  if (hook == NULL) {
+    ret = -1;
+    goto err;
+  }
+  hook->u.func = (void *)hook_func;
+  hook->patch = patch;
+
+  ret = hooks_add_hook(&patch->data.hooks, hook);
+  if (ret < 0 && patch->data.hooks.head == NULL) {
+    LOG("failed to add hook and patch is now empty, freeing hook %p", hook);
+    slab_free(patch->slab, hook);
+    hook = NULL;
+    proc_map_remove(g_map, patch);
+    ksceKernelDeleteUid(patch->uid);
+    patch = NULL;
+  } else if (ret >= 0) {
+    ret = patch->uid;
+    *p_hook = slab_getmirror(patch->slab, hook);
+  }
+
+err:
+  // error and we have allocated a hook
+  if (ret < 0 && patch && hook) {
+    LOG("freeing hook %p", hook);
+    slab_free(patch->slab, hook);
+  }
+
+  ksceKernelUnlockMutex(g_hooks_lock, 1);
+
+  return ret;
+  */
+}
+
 /** @name Kernel Hooks
  * Hooks exports to kernel
  */
 /** @{ */
-EXPORT(SceUID, taiHookFunctionAbs, SceUID pid, tai_hook_ref_t *p_hook, void *dest_func, const void *hook_func) {
+EXPORT(SceUID, taiHookFunctionAbs, SceUID pid, tai_hook_ref_t *p_hook, Ptr<void> dest_func, Ptr<const void> hook_func) {
     TRACY_FUNC(taiHookFunctionAbs, pid, p_hook, dest_func, hook_func);
     return UNIMPLEMENTED();
 }
-EXPORT(SceUID, taiHookFunctionExportForKernel, SceUID pid, tai_hook_ref_t *p_hook, const char *module, uint32_t library_nid, uint32_t func_nid, const void *hook_func) {
+EXPORT(SceUID, taiHookFunctionExportForKernel, SceUID pid, tai_hook_ref_t *p_hook, const char *module, uint32_t library_nid, uint32_t func_nid, Ptr<const void> hook_func) {
     TRACY_FUNC(taiHookFunctionExportForKernel, pid, p_hook, module, library_nid, func_nid, hook_func);
     return UNIMPLEMENTED();
 }
-EXPORT(SceUID, taiHookFunctionImportForKernel, SceUID pid, tai_hook_ref_t *p_hook, const char *module, uint32_t import_library_nid, uint32_t import_func_nid, const void *hook_func) {
+EXPORT(SceUID, taiHookFunctionImportForKernel, SceUID pid, tai_hook_ref_t *p_hook, const char *module, uint32_t import_library_nid, uint32_t import_func_nid, Ptr<const void> hook_func) {
     TRACY_FUNC(taiHookFunctionImportForKernel, pid, p_hook, module, import_library_nid, import_func_nid, hook_func);
-    return UNIMPLEMENTED();
+    // LOG_CONSOLE(taiHookFunctionImportForKernel, pid, p_hook, module, import_library_nid, import_func_nid, hook_func);
+    auto name = import_name(import_func_nid);
+    LOG_TRACE("try to patch {} in {} for module {} (nid:{})", name, log_hex(import_library_nid), module ? module : "main", log_hex(import_func_nid));
+    int ret;
+    Ptr<void> stubptr;
+    uint32_t stub[3];
+
+    ret = module_get_import_func(emuenv, thread_id, pid, module, import_library_nid, import_func_nid, &stubptr);
+    if (ret < 0) {
+        LOG("Failed to find stub for %s, NID:0x%08X: 0x%08X", module, import_func_nid, ret);
+        return ret;
+    }
+    stubptr = Ptr<void>(stubptr.address() & ~1);
+    memcpy(stub, stubptr.get(emuenv.mem), sizeof(stub));
+    /*
+      ret = tai_memcpy_to_kernel(pid, stub, (const void *)(stubptr & ~1), sizeof(stub));
+      if (ret < 0) {
+        LOG("Failed to read stub %p, %x", stubptr, ret);
+        return ret;
+      }
+      // FIXME: find a better way to do this
+      if (stub[0] == 0xE24FC008 && stub[1] == 0xE12FFF1E) {
+        LOG("stub for %p has not been resolved yet!", import_func_nid);
+        return TAI_ERROR_STUB_NOT_RESOLVED;
+      }
+      */
+    return tai_hook_func_abs(emuenv, p_hook, pid, stubptr, hook_func);
+    // taiHookFunctionAbs(pid, p_hook, (void *)stubptr, hook_func);
+
+    // return UNIMPLEMENTED();
 }
 EXPORT(SceUID, taiHookFunctionOffsetForKernel, SceUID pid, tai_hook_ref_t *p_hook, SceUID modid, int segidx, uint32_t offset, int thumb, const void *hook_func) {
     TRACY_FUNC(taiHookFunctionOffsetForKernel, pid, p_hook, modid, segidx, offset, thumb, hook_func);
@@ -413,8 +680,14 @@ EXPORT(SceUID, taiHookFunctionImportForUser, tai_hook_ref_t *p_hook, tai_hook_ar
     // LOG_CONSOLE(taiHookFunctionImportForUser, p_hook, args->module, args->library_nid, args->func_nid, args->hook_func);
     auto name = import_name(args->func_nid);
     LOG_TRACE("try to patch {} (nid:{})", name, log_hex(args->func_nid));
-    return UNIMPLEMENTED();
+    if (args->size != sizeof(*args)) {
+        LOG_ERROR("invalid args size: {}", args->size);
+        return RET_ERROR(TAI_ERROR_USER_MEMORY);
+    }
+    SceUID pid = 1; // CALL_EXPORT(sceKernelGetProcessId);
+    return CALL_EXPORT(taiHookFunctionImportForKernel, pid, p_hook, args->module.get(emuenv.mem), args->library_nid, args->func_nid, args->hook_func);
 }
+
 EXPORT(SceUID, taiHookFunctionOffsetForUser, tai_hook_ref_t *p_hook, tai_offset_args_t *args) {
     TRACY_FUNC(taiHookFunctionOffsetForUser, p_hook, args);
     return UNIMPLEMENTED();
@@ -447,40 +720,7 @@ constexpr int MOD_LIST_SIZE = 256;
 EXPORT(int, taiGetModuleInfo, const char *module_name, tai_module_info_t *info) {
     TRACY_FUNC(taiGetModuleInfo, module_name, info);
     LOG_CONSOLE(taiGetModuleInfo, module_name, info);
-    if (info->size != sizeof(tai_module_info_t)) {
-        LOG_ERROR("Structure size too small: %d", info->size);
-        return TAI_ERROR_SYSTEM;
-    }
-
-    int module_id = 0;
-    if (module_name == nullptr) {
-        for (auto &[module_id_, module] : emuenv.kernel.loaded_modules) {
-            if (module->info.path == "app0:" + emuenv.self_path) {
-                module_id = module_id_;
-            }
-        }
-    } else {
-        for (auto &[module_id_, module] : emuenv.kernel.loaded_modules) {
-            if (strncmp(module->info.module_name, module_name, sizeof(module->info.module_name)) == 0) {
-                module_id = module_id_;
-            }
-        }
-    }
-    if (module_id == 0) {
-        return TAI_ERROR_NOT_FOUND;
-    }
-    auto &module_info = emuenv.kernel.loaded_modules[module_id];
-    const sce_module_info_raw *int_mod_info = reinterpret_cast<const sce_module_info_raw *>(module_info->info_segment_address.get(emuenv.mem) + module_info->info_offset);
-    info->modid = module_id;
-    info->module_nid = int_mod_info->module_nid;
-    info->exports_start = int_mod_info->export_top;
-    info->exports_end = int_mod_info->export_end;
-    info->imports_start = int_mod_info->import_top;
-    info->imports_end = int_mod_info->import_end;
-    strncpy(info->name, int_mod_info->name, 27);
-    info->name[26] = '\0';
-    // LOG_TRACE("taiGetModuleInfo,module:{}",module_name);
-    return 0; // UNIMPLEMENTED();
+    return module_get_by_name_nid(emuenv, 1, module_name, TAI_IGNORE_MODULE_NID, info);
 }
 EXPORT(int, taiHookRelease, SceUID tai_uid, tai_hook_ref_t hook) {
     TRACY_FUNC(taiHookRelease, tai_uid, hook);
